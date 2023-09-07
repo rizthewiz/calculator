63d8a791809f5f808797c07aa21c06bf573a94b5
rizthewiz <rizthewiz>
2023-09-07 19:00:02 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const test = " " == false;
+console.log(test);
+console.log("test");
+console.log(typeof "a");
+console.log(typeof 3);
+console.log(typeof true);
+console.log(typeof 3.5);
+console.log(typeof "jd");
